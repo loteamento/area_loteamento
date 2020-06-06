f0c78b9c41cf4cbaa6d78350c42c30422e88fabb
Scarlet Barbosa <scarlet.profissional@gmail.com>
2020-06-06 13:49:31 -0300

    Projeto Loteamento

---
+++ data/verticesareaTOPOGRAPHcorrigdoline_2.js
@@ -0,0 +1,14 @@
+var json_verticesareaTOPOGRAPHcorrigdoline_2 = {
+"type": "FeatureCollection",
+"name": "verticesareaTOPOGRAPHcorrigdoline_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Layer": "PERIMETRO", "SubClasses": "AcDbEntity:AcDbLine", "ExtendedEn": null, "Linetype": null, "EntityHand": "20050", "Text": null }, "geometry": { "type": "LineString", "coordinates": [ [ -48.429804586750407, -25.628329939291749, 0.0 ], [ -48.430277116919392, -25.628805244582189, 0.0 ] ] } },
+{ "type": "Feature", "properties": { "Layer": "PERIMETRO", "SubClasses": "AcDbEntity:AcDbLine", "ExtendedEn": null, "Linetype": null, "EntityHand": "200FA", "Text": null }, "geometry": { "type": "LineString", "coordinates": [ [ -48.430277116919392, -25.628805244582189, 0.0 ], [ -48.433945862393692, -25.625679897727473, 0.0 ] ] } },
+{ "type": "Feature", "properties": { "Layer": "PERIMETRO", "SubClasses": "AcDbEntity:AcDbLine", "ExtendedEn": null, "Linetype": null, "EntityHand": "201BA", "Text": null }, "geometry": { "type": "LineString", "coordinates": [ [ -48.433945862393692, -25.625679897727473, 0.0 ], [ -48.43445356195673, -25.625247375131277, 0.0 ] ] } },
+{ "type": "Feature", "properties": { "Layer": "PERIMETRO", "SubClasses": "AcDbEntity:AcDbLine", "ExtendedEn": null, "Linetype": null, "EntityHand": "20260", "Text": null }, "geometry": { "type": "LineString", "coordinates": [ [ -48.430197413569516, -25.620966214849982, 0.0 ], [ -48.429804586750407, -25.628329939291749, 0.0 ] ] } },
+{ "type": "Feature", "properties": { "Layer": "PERIMETRO", "SubClasses": "AcDbEntity:AcDbLine", "ExtendedEn": null, "Linetype": null, "EntityHand": "20306", "Text": null }, "geometry": { "type": "LineString", "coordinates": [ [ -48.430358051759285, -25.61795448463014, 0.0 ], [ -48.436161636308981, -25.623792183727186, 0.0 ] ] } },
+{ "type": "Feature", "properties": { "Layer": "PERIMETRO", "SubClasses": "AcDbEntity:AcDbLine", "ExtendedEn": null, "Linetype": null, "EntityHand": "20307", "Text": null }, "geometry": { "type": "LineString", "coordinates": [ [ -48.43445356195673, -25.625247375131277, 0.0 ], [ -48.436161636308981, -25.623792183727186, 0.0 ] ] } },
+{ "type": "Feature", "properties": { "Layer": "PERIMETRO", "SubClasses": "AcDbEntity:AcDbLine", "ExtendedEn": null, "Linetype": null, "EntityHand": "2059B", "Text": null }, "geometry": { "type": "LineString", "coordinates": [ [ -48.430197413569516, -25.620966214849982, 0.0 ], [ -48.430358051759285, -25.61795448463014, 0.0 ] ] } }
+]
+}
---
+++ data/verticesareaTOPOGRAPHcorrigdopoint_3.js
@@ -0,0 +1,14 @@
+var json_verticesareaTOPOGRAPHcorrigdopoint_3 = {
+"type": "FeatureCollection",
+"name": "verticesareaTOPOGRAPHcorrigdopoint_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Layer": "VERTICES", "SubClasses": "AcDbEntity:AcDbPoint", "ExtendedEn": null, "Linetype": null, "EntityHand": "20000", "Text": "P3" }, "geometry": { "type": "Point", "coordinates": [ -48.429804586750407, -25.628329939291749, 0.0 ] } },
+{ "type": "Feature", "properties": { "Layer": "VERTICES", "SubClasses": "AcDbEntity:AcDbPoint", "ExtendedEn": null, "Linetype": null, "EntityHand": "20001", "Text": "P1" }, "geometry": { "type": "Point", "coordinates": [ -48.430358051759285, -25.61795448463014, 0.0 ] } },
+{ "type": "Feature", "properties": { "Layer": "VERTICES", "SubClasses": "AcDbEntity:AcDbPoint", "ExtendedEn": null, "Linetype": null, "EntityHand": "20002", "Text": "P5" }, "geometry": { "type": "Point", "coordinates": [ -48.433945862393692, -25.625679897727473, 0.0 ] } },
+{ "type": "Feature", "properties": { "Layer": "VERTICES", "SubClasses": "AcDbEntity:AcDbPoint", "ExtendedEn": null, "Linetype": null, "EntityHand": "20003", "Text": "P4" }, "geometry": { "type": "Point", "coordinates": [ -48.430277116919392, -25.628805244582189, 0.0 ] } },
+{ "type": "Feature", "properties": { "Layer": "VERTICES", "SubClasses": "AcDbEntity:AcDbPoint", "ExtendedEn": null, "Linetype": null, "EntityHand": "201BB", "Text": "P6" }, "geometry": { "type": "Point", "coordinates": [ -48.43445356195673, -25.625247375131277, 0.0 ] } },
+{ "type": "Feature", "properties": { "Layer": "VERTICES", "SubClasses": "AcDbEntity:AcDbPoint", "ExtendedEn": null, "Linetype": null, "EntityHand": "20308", "Text": "P7" }, "geometry": { "type": "Point", "coordinates": [ -48.436161636308981, -25.623792183727186, 0.0 ] } },
+{ "type": "Feature", "properties": { "Layer": "VERTICES", "SubClasses": "AcDbEntity:AcDbPoint", "ExtendedEn": null, "Linetype": null, "EntityHand": "20452", "Text": "P2" }, "geometry": { "type": "Point", "coordinates": [ -48.430197413569516, -25.620966214849982, 0.0 ] } }
+]
+}
